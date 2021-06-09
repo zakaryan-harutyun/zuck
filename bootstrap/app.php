dfa7ee862d7a9dbd06a86c2b6fbe56435ea2b86e
Zakaryan Harutyun <quite1difficult@gmail.com>
2021-06-09 23:29:39 +0400

    emal/pdf configs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,6 +79,20 @@ $app->singleton(
  $app->register(App\Providers\AppServiceProvider::class);
  $app->register(App\Providers\AuthServiceProvider::class);
  $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
+$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
+
+
+
+$app->configure('mail');
+
+$app->alias('mail.manager', Illuminate\Mail\MailManager::class);
+$app->alias('mail.manager', Illuminate\Contracts\Mail\Factory::class);
+
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
+
 
 /*
 |--------------------------------------------------------------------------
--- database/migrations/2021_06_09_191226_create_formdata_table.php
+++ database/migrations/2021_06_09_191226_create_formdata_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDatasTable extends Migration
+class CreateFormdataTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateDatasTable extends Migration
      */
     public function up()
     {
-        Schema::create('datas', function (Blueprint $table) {
+        Schema::create('formdata', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email');
-            $table->string('body');
-            $table->string('name');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateDatasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('datas');
+        Schema::dropIfExists('formdata');
     }
 }
